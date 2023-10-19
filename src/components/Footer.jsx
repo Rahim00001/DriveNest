7dfb492bffc721231f3517fd8afa0266df6e652a
Rahim00001 <rahimlpt2022@gmail.com>
2023-10-19 15:17:07 +0600

    basic footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-
+import driveNest from "../../public/DriveNest.png"
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className="footer footer-center p-10 bg-base-100 text-primary-content">
+            <aside>
+                <a href="/"><img className="w-20" src={driveNest} alt="" /></a>
+                <p className="font-bold text-lg text-black -mt-5">DriveNest Ltd. </p>
+                <p className="text-black">Providing reliable cars since 1992</p>
+                <p className="text-black">Copyright © 2023 - All right reserved</p>
+            </aside>
+        </footer>
     );
 };
 
